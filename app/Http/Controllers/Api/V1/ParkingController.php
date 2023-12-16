9961f03a3ddc21d2b81da7b5388ee67eabb2a36c
kazburrel <oegbosionu@gmail.com>
2023-12-16 18:27:19 +0100

    User Parking Zone

--- app/Http/Controllers/Api/V1/ParkingController.php
+++ app/Http/Controllers/Api/V1/ParkingController.php
@@ -37,4 +37,13 @@ class ParkingController extends Controller
     {
         return ParkingResource::make($parking);
     }
+
+    public function stop(Parking $parking)
+{
+    $parking->update([
+        'stop_time' => now(),
+    ]);
+ 
+    return ParkingResource::make($parking);
+}
 }
