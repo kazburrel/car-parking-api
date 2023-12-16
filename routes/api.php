cf772a97408940cda01aafcfd6afd8cffcd84a48
kazburrel <oegbosionu@gmail.com>
2023-12-16 12:41:18 +0100

    User Profile Update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Controllers\Api\V1\Auth\PasswordUpdateController;
 use App\Http\Controllers\Api\V1\Auth\ProfileController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use Illuminate\Http\Request;
@@ -28,4 +29,5 @@ Route::post('auth/login', LoginController::class);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [ProfileController::class, 'show']);
     Route::put('profile', [ProfileController::class, 'update']);
+    Route::put('password', PasswordUpdateController::class);
 });
