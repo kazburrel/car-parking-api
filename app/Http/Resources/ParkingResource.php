929f61e7523f6f6be71e8d956fbe73622a56058c
kazburrel <oegbosionu@gmail.com>
2023-12-16 13:32:50 +0100

    User Parking Zone

--- app/Http/Resources/ParkingResource.php
+++ app/Http/Resources/ParkingResource.php
@@ -14,6 +14,18 @@ class ParkingResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'zone' => [
+                'name' => $this->zone->name,
+                'price_per_hour' => $this->zone->price_per_hour,
+            ],
+            'vehicle' => [
+                'plate_number' => $this->vehicle->plate_number
+            ],
+            'start_time' => $this->start_time->toDateTimeString(),
+            'stop_time' => $this->stop_time?->toDateTimeString(),
+            'total_price' => $this->total_price,
+        ];
     }
 }
